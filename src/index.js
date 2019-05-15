668c4a9f272ed3367d7331ac629dcb51d0567a0e
Victoria Mei <vm930@hotmail.com>
2019-05-15 17:19:23 -0400

    add redux and init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducer/index.js
@@ -0,0 +1,8 @@
+const initialState = 'im legit';
+
+function reducer(state, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
