aa0338fa090a21e05173a3c792b75f8d3b3967e4
Victoria Mei <vm930@hotmail.com>
2019-05-20 22:34:52 -0400

    added hyperlink not yet working but can keep it as stretch goal later

--- src/containers/EditorIndex.js
+++ src/containers/EditorIndex.js
@@ -4,5 +4,6 @@ import ItalicMark from './ItalicMark';
 import Underline from './Underline';
 import Code from './Code';
 import List from './List';
+import Link from './Link';
 import FormatToolbar from './FormatToolbar';
-export { TextEditor, BoldMark, ItalicMark, Underline, Code, List, FormatToolbar };
+export { TextEditor, BoldMark, ItalicMark, Underline, Code, List, Link, FormatToolbar };
---
+++ src/containers/Link.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Link = (props) => {
+	return <link {...props.attributes}>{props.children}</link>;
+};
+
+export default Link;
